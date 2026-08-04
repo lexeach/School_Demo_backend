2ed588577ab219e4df141cd0c4d2c22d7d693996
lexeach <80155340+lexeach@users.noreply.github.com>
2026-08-04 22:45:47 +0530

    Update seedAdmin.js

--- utils/seedAdmin.js
+++ utils/seedAdmin.js
@@ -1,49 +1,38 @@
-const bcrypt = require("bcryptjs");
 const User = require("../models/user.model");
 
 const seedAdmin = async () => {
   try {
-    // Check if admin already exists
-    const adminExists = await User.findOne({
-      role: "admin",
-    });
+    const adminExists = await User.findOne({ role: "admin" });
 
     if (adminExists) {
       console.log("✅ Admin already exists");
       return;
     }
 
-    // Read from environment variables
     const adminName = process.env.ADMIN_NAME;
     const adminEmail = process.env.ADMIN_EMAIL;
     const adminPassword = process.env.ADMIN_PASSWORD;
 
-    // Validate env variables
     if (!adminName || !adminEmail || !adminPassword) {
       console.log("⚠️ Admin environment variables are missing.");
       return;
     }
 
-    // Encrypt password
-    const hashedPassword = await bcrypt.hash(adminPassword, 10);
-
-    // Create admin
     await User.create({
       name: adminName,
       email: adminEmail,
-      password: hashedPassword,
+      password: adminPassword, // plain password
       role: "admin",
       isVerified: true,
       isActive: true,
     });
 
-    console.log("======================================");
-    console.log("🎉 Default Admin Created Successfully");
-    console.log("Email :", adminEmail);
-    console.log("======================================");
-  } catch (error) {
-    console.error("❌ Admin Seeder Error");
-    console.error(error);
+    console.log("==================================");
+    console.log("🎉 Default Admin Created");
+    console.log("Email:", adminEmail);
+    console.log("==================================");
+  } catch (err) {
+    console.error("❌ Seed Admin Error:", err);
   }
 };
 
