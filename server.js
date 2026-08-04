d619a4f9ef5ce7b43597dddea97bafd04ac27050
lexeach <80155340+lexeach@users.noreply.github.com>
2026-08-04 21:43:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const allowedOrigins =
     "https://user.exowa.click",
     "https://test.exowa.click",
     "https://heartfelt-dolphin-732aa6.netlify.app",
-    "https://schoolname.exowa.click",
+    "https://exowa-school-demo.netlify.app",
     "https://svn.exowa.click",
   "https://auto-paper.netlify.app", // Deployed host
   "http://localhost:5173", // Local development
