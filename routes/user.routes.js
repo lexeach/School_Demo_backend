76d60833d94ca209286bd8b2b7e7fb0d22b64b55
lexeach <80155340+lexeach@users.noreply.github.com>
2026-08-10 20:20:46 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,9 +7,15 @@ const {
   loginUser,
   getProfile,
   updateProfile,
+  bulkUploadUsers,
 } = require("../controllers/user.controller");
 
-const { protect } = require("../middleware/auth");
+const {
+  protect,
+  adminAuth,
+} = require("../middleware/auth");
+
+const excelUpload = require("../middleware/excelUpload");
 
 /**
  * Public Routes
@@ -30,5 +36,18 @@ router.get("/profile", protect, getProfile);
 
 // Update Profile
 router.put("/profile", protect, updateProfile);
+
+/**
+ * Admin Routes
+ *
+ * Import users + one child per row from Excel.
+ */
+router.post(
+  "/bulk-upload",
+  protect,
+  adminAuth,
+  excelUpload.single("file"),
+  bulkUploadUsers
+);
 
 module.exports = router;
